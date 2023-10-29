4849599c79fd9e5b3cfd833fbe8154d58f931094
alokaarya4 <alokkumar01104@gmail.com>
2023-10-29 11:53:34 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import './App.css'
+import Main from './components/main';
+import Home from './components/main/Home';
+import Login from './components/main/login';
+
+function App() {
+
+  return (
+    <>
+       <BrowserRouter>
+       <Routes>
+        <Route path='main' element={<Main />}>
+          <Route path='home' element={<Home />} />
+          <Route path='login' element={<Login />} />
+        
+        <Route path='user' element={<Main />}>
+          <Route path='home' element={<Home />} />
+        
+        
+        <Route path='admin' element={<Main />}>
+          <Route path='home' element={<Home />} />
+          <Route path='login' element={<Login />} />
+          
+        </Route>
+       </Routes>
+       </BrowserRouter>
+    </>
+
+  )
+}
+      
+
+export default App
